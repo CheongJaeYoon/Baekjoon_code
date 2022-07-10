00fbacdd84f835e980ffeb17f2f36dc297c84fee
Cheong JaeYoon <jyjung9910@ajou.ac.kr>
2022-07-10 20:59:57 +0900

    1035 not complete

---
+++ gold/1035.js
@@ -0,0 +1,134 @@
+// 시간초과 에러
+
+
+const solution = () => {
+    let start_bit = 0;
+    totalnum = 0;
+    for(let i = 0; i<5; i++){
+        for(let j = 0; j<5; j++){
+            if(data[i][j] == '*'){
+                start_bit = start_bit | (1 << (i*5 + j));
+                totalnum++;
+            }
+        }
+    }
+    func(start_bit, 0)
+}
+
+const func = (bit, count) => { //BFS로 구현
+    let queue = [];
+    queue.push( [bit, count] )
+
+    while(queue.length != 0){
+        let current_bit = queue[0][0];
+        let current_count = queue.shift()[1];
+        let row;
+        let col;
+        
+        if(bitArr[current_bit] != -1){ //같은 경로가 있는 경우
+            continue;
+        }
+        if(connect_all(current_bit)){ //모두 연결이 되었을 경우 정답 제출후 탐색 종료
+            console.log(current_count);
+            break;
+        }
+        for(let i = 0 ; i < 25; i++){
+            if(current_bit & (1 << i)){
+                row = Math.floor(i/5);
+                col = i%5;
+                if( ( (row - 1) != -1) && ( (current_bit & (1 << (row - 1)*5+col)) == 0) ){ //상
+                    queue.push( [ ( current_bit - (1<< i) + (1<< (row-1)*5+col) ) , current_count + 1 ] )
+                }
+                if( ( (row + 1) != 5) && ( (current_bit & (1 << (row + 1)*5+col)) == 0) ){ //하
+                    queue.push( [ ( current_bit - (1<< i) + (1<< (row+1)*5+col) ) , current_count + 1 ] )
+                }
+                if( ( (col - 1) != -1) && ( (current_bit & (1 << row*5+(col - 1) )) == 0) ){ //좌
+                    queue.push( [ ( current_bit - (1<< i) + (1<< row*5+(col-1)) ) , current_count + 1 ] )
+                }
+                if( ( (col + 1) != 5) && ( (current_bit & (1 << row*5+(col + 1) )) == 0) ){ //우
+                    queue.push( [ ( current_bit - (1<< i) + (1<< row*5+(col+1)) ) , current_count + 1 ] )
+                }
+            }
+        }
+        bitArr[current_bit] = current_count
+    }
+}
+const connect_all = (bit) => {
+    let visited = new Array(25).fill(0)
+    let group = [];
+    let count = 0;
+    for(let i = 0 ; i < 25; i++){
+        if(bit & (1 << i)){
+            visited[i] = 1;
+            group.push(i);
+            count++;
+            break;
+        }
+    }
+    while(true){
+        let check = true;
+        while(group.length != 0){
+            let row = Math.floor(group[0]/5);
+            let col = group.shift()%5;
+            if( ( (row - 1) != -1) && ( (bit & (1 << (row - 1)*5+col)) != 0) ){ //상
+                if(!visited[(row - 1)*5+col]){
+                    visited[(row - 1)*5+col] = 1;
+                    group.push((row - 1)*5+col)
+                    count++;
+                    check = false;
+                }
+            }
+            if( ( (row + 1) != 5) && ( (bit & (1 << (row + 1)*5+col)) != 0) ){ //하
+                if(!visited[(row + 1)*5+col]){
+                    visited[(row + 1)*5+col] = 1;
+                    group.push((row + 1)*5+col)
+                    count++;
+                    check = false;
+                }
+            }
+            if( ( (col - 1) != -1) && ( (bit & (1 << row*5+(col - 1) )) != 0) ){ //좌
+                if(!visited[row*5+col -1]){
+                    visited[row*5+col -1] = 1;
+                    group.push(row*5+col -1)
+                    count++;
+                    check = false;
+                }
+            }
+            if( ( (col + 1) != 5) && ( (bit & (1 << row*5+(col + 1) )) != 0) ){ //우
+                if(!visited[row*5+col +1]){
+                    visited[row*5+col +1] = 1;
+                    group.push(row*5+col +1)
+                    count++;
+                    check = false;
+                }
+            }
+        }
+        if(check)
+            break;
+    }
+    if(totalnum == count){
+        return true;
+    }
+    else{
+        return false;
+    }
+    
+}
+
+const fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+
+
+
+const data = [];
+for(let i = 0; i < 5 ; i++){
+    data.push(input[i].split(''));
+}
+
+
+
+const bitArr = new Array(1 << 25).fill(-1);
+let totalnum;
+
+solution();
