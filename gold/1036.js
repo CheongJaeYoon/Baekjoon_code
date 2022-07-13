4887792d3b1f204d445932cab6ffa905956faf26
jaeyoon Cheong <jyjung9910@gmail.com>
2022-07-13 19:08:46 +0900

    1036 complete

--- gold/1036.js
+++ gold/1036.js
@@ -1,17 +1,17 @@
 const solution = (N, data, K) => {
     let number = new Array(36).fill(0);
-    let sum = 0;
+    let sum = 0n;
     for(let i = 0; i<N; i++){
         let len = data[i].length
         for(let j = 0; j < len; j++){
             let tmp = parseInt(data[i][j], 36);
             if(number[tmp]){
-                sum += Math.pow(36, len - j - 1) * tmp;
-                number[tmp] += (Math.pow(36, len - j - 1) * (35 - tmp));
+                sum += bigintPow(36, len - j - 1) * BigInt(tmp);
+                number[tmp] += bigintPow(36, len - j - 1) * BigInt((35 - tmp));
             }
             else{
-                sum += Math.pow(36, len - j - 1) * tmp;
-                number[tmp] = (Math.pow(36, len - j - 1) * (35 - tmp));
+                sum += bigintPow(36, len - j - 1) * BigInt(tmp);
+                number[tmp] = bigintPow(36, len - j - 1) * BigInt((35 - tmp));
             }
         }
     }
@@ -21,13 +21,21 @@ const solution = (N, data, K) => {
         for(let j = 0; j < 36; j++){
             if(number[j] > max[1]){
                 max[0] = j;
-                max[1] = number[j]
+                max[1] = BigInt(number[j])
             }
         }
-        sum += max[1];
+        sum += BigInt(max[1]);
         number[max[0]] = 0;
     }
     console.log(sum.toString(36).toUpperCase());
+}
+
+const bigintPow = (a, b) => {
+    let result = 1n;
+    for(let i = 0; i< b; i++){
+        result = result * BigInt(a)
+    }
+    return result
 }
 
 const fs = require('fs');
