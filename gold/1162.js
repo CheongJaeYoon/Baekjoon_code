749958dd8c0542147cb7b90f593a057ea90ec4cb
Cheong JaeYoon <jyjung9910@ajou.ac.kr>
2022-07-27 00:29:35 +0900

    1162 미해결

--- gold/1162.js
+++ gold/1162.js
@@ -1,18 +1,22 @@
+//오늘의 깨달음 Node에서는 Shift를 사용하면 느려진다... head와 tail을 사용하도록 하자.
+//그냥 Node로 못푸는 문제인듯 하다...
+
 const fs = require('fs');
 let input = fs.readFileSync('./dev/stdin').toString().split('\n');
 
-const num = input[0].replace(/\r/, "").split(" ").map(el => +el);
+const num = input[0].trim().split(" ").map(el => +el);
 const N = num[0];
 const M = num[1];
 const K = num[2];
 const INF = -1;
+const MAX_SIZE = 99999;
 
 let data = new Array(N + 1);
 for(let i= 0; i <= N; i++){
     data[i] = [];
 }
 for(let i = 1; i <= M; i++){
-    let temp = input[i].replace(/\r/, "").split(" ").map(el => +el)
+    let temp = input[i].trim().split(" ").map(el => +el)
     data[temp[0]].push([temp[1], temp[2]]);
     data[temp[1]].push([temp[0], temp[2]]);
 }
@@ -28,15 +32,18 @@ const solution = () => {
 }
 
 const func = (start_node, end_node) => {
-
+    let head = 0;
+    let tail = 0;
     dp[start_node][0] = 0;
 
-    let queue = [];
-    queue.push([0, 1, 0]) // [dist, node, K]
+    let queue = new Array(MAX_SIZE+1);
+    queue[tail] = [0, 1, 0] // [dist, node, K]
+    tail = tail > MAX_SIZE ? 0 : tail + 1;
 
-    while(queue.length != 0){
+    while(head != tail){
         
-        let tmp = queue.shift();
+        let tmp = queue[head];
+        head = head > MAX_SIZE ? 0 : head + 1;
         let cur_dist = tmp[0]
         let cur_node = tmp[1]
         let cur_k = tmp[2]
@@ -52,13 +59,15 @@ const func = (start_node, end_node) => {
             if(cur_k < K){
                 if(dp[next_node][cur_k + 1] > cur_dist || dp[next_node][cur_k + 1] == INF){
                     dp[next_node][cur_k + 1] = cur_dist;
-                    queue.push([cur_dist, next_node, cur_k + 1])
+                    queue[tail] = [cur_dist, next_node, cur_k + 1]
+                    tail = tail > MAX_SIZE ? 0 : tail + 1;
                 }
             }
 
             if(dp[next_node][cur_k] > next_dist || dp[next_node][cur_k] == INF){
                 dp[next_node][cur_k] = next_dist;
-                queue.push([next_dist, next_node, cur_k])
+                queue[tail] = [next_dist, next_node, cur_k];
+                tail = tail > MAX_SIZE ? 0 : tail + 1;
             }
         }
     }
