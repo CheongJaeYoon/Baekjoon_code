9ab9456f3b3084f4182fbda913d34768f2b8bb5d
Cheong JaeYoon <jyjung9910@ajou.ac.kr>
2022-08-05 19:22:55 +0900

    1219 시간초과

--- gold/1219.js
+++ gold/1219.js
@@ -1,13 +1,52 @@
 //node는 BFS를 사용하면 시간이 오래걸린다...
 const fs = require('fs');
 let input = fs.readFileSync('./dev/stdin').toString().split('\n');
+class Node {
+    constructor(node) {
+      this.node = node;
+      this.next = null;
+    }
+  }
+  
+class Queue {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
+
+  push(node) {
+    if (!this.head) {
+      this.head = node;
+    }
+    if (this.tail) {
+      this.tail.next = node;
+    }
+    this.tail = node;
+    this.size++;
+  }
+
+  shift() {
+    if (!this.size) {
+      return null;
+    }
+    const node = this.head;
+    this.head = this.head.next;
+    this.size--;
+    return node;
+  }
+}
+
 
 const num = input[0].trim().split(" ").map(el => +el);
 const N = num[0];
 const start_city = num[1];
 const end_city = num[2];
 const M = num[3];
-const INF = "INF"
+
+const INF = 99999999999;
+const queue = new Queue();
+
 let data = [];
 for(let i = 1; i <= M; i++){
     data.push(input[i].trim().split(" ").map(el => +el))
@@ -19,8 +58,7 @@ for(let i = 0;i < N; i++){
     graph[i] = []
 }
 for(let i = 0; i < M; i++){
-    data[i][2] = data[i][2] - data2[data[i][1]]
-    graph[data[i][0]].push(data[i][1])
+    graph[data[i][0]].push([data[i][1], data[i][2] - data2[data[i][1]]])
 }
 
 let table = new Array(N).fill(INF);
@@ -30,20 +68,23 @@ let result;
 const solution = () => {
     table[start_city] = -data2[start_city];
     for(let i = 0; i < N; i++){
-        for(let j = 0; j < M; j++){
-            cur_node = data[j][0];
-            next_node = data[j][1];
-            cost = data[j][2];
-            if(table[cur_node] != INF && (table[next_node] == INF || table[next_node] > table[cur_node] + cost)){
-                table[next_node] = table[cur_node] + cost
-                if(i == N-1){
-                    if(gee_check(next_node)){
-                        result = "Gee"
-                        return;
+        for(let j = 0; j < N; j++){
+            if(table[j] == INF)
+                continue;
+            for(let k = 0; k < graph[j].length; k++){
+                let cur_node = j;
+                let next_node = graph[j][k][0];
+                let cost = graph[j][k][1];
+                if((table[cur_node] != INF) && (table[next_node] > table[cur_node] + cost)){
+                    table[next_node] = table[cur_node] + cost
+                    if(i == N-1){
+                        if(gee_check(next_node)){
+                            result = "Gee"
+                            return;
+                        }
                     }
                 }
             }
-
         }
     }
     if(table[end_city] == INF)
@@ -51,18 +92,19 @@ const solution = () => {
     else
         result = -table[end_city];
 }
-
 const gee_check = (node) => {
     let visit = new Array(N).fill(0);
-    let queue = [node];
+    
+    queue.push(new Node(node))
     visit[node] = 1;
-    while(queue.length != 0){
-        let tmp = queue.shift();
+    while(queue.size){
+        const tmp = queue.shift().node;
         if(tmp == end_city)
             return true;
         for(let i = 0; i < graph[tmp].length; i++){
-            if(visit[graph[tmp][i]] == 0)
-                queue.push(graph[tmp][i]);
+            if(visit[graph[tmp][i][0]] == 0){
+                queue.push(new Node(graph[tmp][i][0]))
+            }
         }
     }
     return false;
@@ -71,3 +113,4 @@ const gee_check = (node) => {
 
 solution();
 console.log(result)
+
