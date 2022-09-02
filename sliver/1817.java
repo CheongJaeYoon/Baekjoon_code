567a121abd44eedae76dce0bbbae2345e315a140
Cheong JaeYoon <jyjung9910@ajou.ac.kr>
2022-09-02 20:01:49 +0900

    1817 complete

--- sliver/1817.java
+++ sliver/1817.java
@@ -11,11 +11,32 @@ public class Main {
         String[] input_first = br.readLine().split(" ");
         int n = Integer.parseInt(input_first[0]);
         int m = Integer.parseInt(input_first[1]);
+        if(n == 0){
+            System.out.println(0);
+            return; 
+        }
         String[] input_second = br.readLine().split(" ");
         int book[] = new int[n];
         for(int i = 0; i < n; i++){
             book[i] = Integer.parseInt(input_second[i]);
         }
-        System.out.print(Arrays.toString(book));
+        int cnt = 0;
+        int index = 0;
+        while(true){
+            if(index == n)
+                break;
+            int sum = 0;
+            while(true){
+                sum += book[index];
+                if(sum > m)
+                    break;
+                index++;
+                if(index == n)
+                    break;
+
+            }
+            cnt++;
+        }
+        System.out.println(cnt);
     }
 }
