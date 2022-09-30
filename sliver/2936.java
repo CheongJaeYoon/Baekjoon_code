aa93e7489dbdd13f6b312f733d4af767e9ab06d6
Cheong JaeYoon <jyjung9910@gmail.com>
2022-09-30 22:57:19 +0900

    2936 complete

--- sliver/2936.java
+++ sliver/2936.java
@@ -10,23 +10,56 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         st = new StringTokenizer(br.readLine());
-        int x = Integer.parseInt(st.nextToken());
-        int y = Integer.parseInt(st.nextToken());
-        double g_x = 250/3;
-        double g_y = 250/3;
+        double x = Double.parseDouble(st.nextToken());
+        double y = Double.parseDouble(st.nextToken());
+        double r_x = 0;
+        double r_y = 0;
 
-        if(x + y <= 250){
-
+        if(x + y <= 125){
+            if(x > 0){
+                r_y = (250 * 250 / 2) / (250 - x);
+                r_x = 250 - r_y; 
+            }
+            else if(y >= 0){
+                r_x = (250 * 250 / 2) / (250 - y);
+                r_y = 250 - r_x; 
+            }
+            else{
+                System.out.println("error1");
+            }
         }
         else if(y - x >= 0){
-
+            if(x == 0){
+                r_x = (250 * 250 / 2) / y;
+                r_y = 0;
+            }
+            else if(x > 0){
+                r_x = 250 - (250 * 250 / 2) / y;
+                r_y = 0;
+            }
+            else{
+                System.out.println("error2");
+            }
         }
         else if(y - x <= 0){
+            if(y > 0){
+                r_y = 250 - (250 * 250 / 2) / x;
+                r_x = 0;
 
+            }
+            else if(y == 0){
+                r_y = (250 * 250 / 2) / x;
+                r_x = 0;
+
+            }
+            else{
+                System.out.println("error3");
+            }
         }
         else{
-            System.out.println("error");
+            System.out.println("error4");
         }
+        System.out.println(String.format("%.2f", r_x) +  " " + String.format("%.2f", r_y));
     }
 
 
