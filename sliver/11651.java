3efca207dddf65f2c995da33dbd1ddd9bcc8cff8
Cheong JaeYoon <jyjung9910@gmail.com>
2022-11-06 10:04:43 +0900

    11651 complete

---
+++ sliver/11651.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.Math;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException  {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        ArrayList<Dot> dots = new ArrayList<>();
+        while(n-- > 0){
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            dots.add(new Dot(x, y));
+        }
+        Collections.sort(dots, new Comparator<Dot>() {
+            @Override
+            public int compare(Dot a, Dot b){
+                if(a.getY() == b.getY()){
+                    return a.getX() - b.getX();
+                }
+                else{
+                    return a.getY() - b.getY();
+                }
+            }
+            
+        });
+        for(Dot tmp : dots){
+            System.out.println(tmp.getX() + " " + tmp.getY());
+        }
+
+    }
+
+
+}
+
+class Dot{
+    private int x;
+    private int y;
+    public Dot(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+    public int getX() {
+        return this.x;
+    }
+    public int getY() {
+        return this.y;
+    }
+}
