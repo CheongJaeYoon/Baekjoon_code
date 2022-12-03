99419a93d93ca79e5527f38f4291662c1cdddc7b
Cheong JaeYoon <jyjung9910@gmail.com>
2022-12-03 13:11:53 +0900

    11726 complete

---
+++ sliver/11726.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+import java.math.BigInteger;
+
+class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int sum = 0;
+        for(int i = 0; i <= n/2; i++){
+            int total = n - i;
+            sum += combination(total, i);
+            sum = sum % 10007;
+        }
+        System.out.println(sum);
+    }
+    public static int combination(int n, int k){
+        BigInteger a = BigInteger.valueOf(1);
+        BigInteger b = BigInteger.valueOf(1);
+        for(int i = n; i > n-k; i--){
+            a = a.multiply(BigInteger.valueOf(i));
+        }
+        for(int i = 1; i <= k; i++){
+            b = b.multiply(BigInteger.valueOf(i));
+        }
+        BigInteger c = a.divide(b);
+        c = c.mod(BigInteger.valueOf(10007));
+        //System.out.println(n + " " + k + " " + a.toString() + " " + b.toString() + " " + c.toString());
+        return c.intValue();
+    }
+}
