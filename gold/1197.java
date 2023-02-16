c29b91c57fdf75c4271cf6c938b8396fefc10202
Cheong JaeYoon <jyjung9910@gmail.com>
2023-02-16 20:43:47 +0900

    1197 complete

---
+++ gold/1197.java
@@ -0,0 +1,55 @@
+import java.util.*;
+import java.io.*;
+import java.lang.Math;
+
+public class Main{
+    public static PriorityQueue<int[]> edge;
+    public static int[] parent;
+    public static int v, e;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        v = Integer.parseInt(st.nextToken());
+        e = Integer.parseInt(st.nextToken());
+
+        edge = new PriorityQueue<>((x, y) -> { return x[2] - y[2]; });
+        
+        for(int i = 0; i < e; i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            edge.add(new int[]{a, b, c});
+        }
+        
+        parent = new int[v+1];
+        for(int i = 1; i <= v; i++){
+            parent[i] = i;
+        }
+
+
+        int sum = 0;
+        while(!edge.isEmpty()){
+            int[] tmp = edge.poll();
+            if(find(tmp[0]) != find(tmp[1])){
+                sum += tmp[2];
+                union(tmp[0], tmp[1]);
+            }
+        }
+
+        System.out.println(sum);
+    }
+
+    public static int find(int v){
+        if(parent[v] == v) return v;
+        return parent[v] = find(parent[v]);
+    }
+
+    public static void union(int v1, int v2){
+        v1 = find(v1);
+        v2 = find(v2);
+        if(v1 != v2)
+            parent[v2] = v1;
+    }
+}
